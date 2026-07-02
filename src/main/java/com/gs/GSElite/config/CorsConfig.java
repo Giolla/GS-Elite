c35716b3286b8031515d614dbd11c511c4fca9b8
Giolla <sukhitashvili@gmail.com>
2026-07-02 11:21:55 -0400

    Update CorsConfig.java

--- src/main/java/com/gs/GSElite/config/CorsConfig.java
+++ src/main/java/com/gs/GSElite/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "https://elite-giolla.web.app",
                         "https://elite-giolla.firebaseapp.com",
